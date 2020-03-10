f63a69f23e665c1908591a434250c09a920acbf1
JoshArendse <42963960+JoshArendse@users.noreply.github.com>
2020-03-10 13:07:32 +0200

    page concept
    Added page concept design for projects section and improved viewport

--- js/main.js
+++ js/main.js
@@ -21,7 +21,36 @@ button.addEventListener('click', function(){
 window.addEventListener('click', function(c){
 
   if (c.target == popup) {
-    popup.style.display = "none";
+    popup.style.display = 'none';
   }
 
 })
+
+// page concept
+
+$(document).ready(function(){
+    $('#page1').click(function(){
+        $('.projects').toggleClass('block');
+        $('#page1').toggleClass('active');
+        $('#page2').toggleClass('steekhomweg');
+        $('#page3').toggleClass('steekhomweg');
+    })
+});
+
+$(document).ready(function(){
+  $('#page2').click(function(){
+      $('.projects').toggleClass('block');
+      $('#page2').toggleClass('active');
+      $('#page1').toggleClass('steekhomweg');
+      $('#page3').toggleClass('steekhomweg');
+  })
+});
+
+$(document).ready(function(){
+  $('#page3').click(function(){
+      $('.projects').toggleClass('block');
+      $('#page3').toggleClass('active');
+      $('#page2').toggleClass('steekhomweg');
+      $('#page1').toggleClass('steekhomweg');
+  })
+});
